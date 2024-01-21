7fc1123956a2704ad2b05111fd4a381b85912041
Hector Cryo <sonic200887@gmail.com>
2024-01-21 13:21:33 +0200

    Added foundational code to set up the structure and basic design of the website

--- main.js
+++
@@ -1,9 +0,0 @@
-let h1 = document.body.querySelector('h1');
-
-const createP = () => {
-    let paragraph = document.createElement('p');
-    paragraph.innerHTML = 'I am a paragraph from JS';
-    document.body.appendChild(paragraph);
-}
-
-h1.onclick = createP;
